5051d4322f0910e9650ba731c29dff53e992b2c0
Diego <diego.trabalhodev@gmail.com>
2025-02-18 01:53:08 -0300

    quarto commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,20 +1,16 @@
     function openTab(tabName) {
-        // Esconde todos os conteúdos
         var contents = document.querySelectorAll('.tab-content');
         contents.forEach(function(content) {
         content.classList.remove('active-content');
         });
     
-        // Remove a classe ativa de todas as abas
         var tabs = document.querySelectorAll('.tab');
         tabs.forEach(function(tab) {
         tab.classList.remove('active-tab');
         });
-    
-        // Mostra o conteúdo da aba selecionada
+
         document.getElementById(tabName).classList.add('active-content');
     
-        // Marca a aba como ativa
         var activeTab = document.querySelector('.tab[onclick="openTab(\'' + tabName + '\')"]');
         activeTab.classList.add('active-tab');
     }
